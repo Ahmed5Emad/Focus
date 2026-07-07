1d1012d6ae3426141be284c9bb62fab2173d6ea5
Ahmed Emad <ahmed2077emad@gmail.com>
2026-07-07 16:33:15 +0300

    fix: non-null assertion on user in Goals fetch

--- src/App/Pages/Goals/Goals.tsx
+++ src/App/Pages/Goals/Goals.tsx
@@ -34,7 +34,7 @@ export default function Goals() {
         .from("goals")
         .select("*")
         .eq("workspace_id", currentWorkspaceId)
-        .eq("user_id", user.id)
+        .eq("user_id", user!.id)
         .order("created_at", { ascending: false });
 
       if (error) throw error;
